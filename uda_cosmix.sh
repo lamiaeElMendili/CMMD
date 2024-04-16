bfd02c85fc3557c1d297fe6bd80846864190f37b
lamiaeel <lamiae.elmendili@gmail.com>
2024-04-16 11:33:14 -0400

    running scripts

--- uda_cosmix.sh
+++ uda_cosmix.sh
@@ -49,14 +49,14 @@ wandb login 1452c1f31edcc38f0d34fc5ab926714acf4c4df9
 
 pip uninstall opencv-python -y
 pip install opencv-python>=4.5.4.58 --no-index
-pip install nuscenes_devkit-1.1.9-py3-none-any.whl
+pip install --no-index nuscenes_devkit-1.1.9-py3-none-any.whl 
 
 
 cd CMMD
 
 #python adapt_cosmix.py --config_file configs/adaptation/synlidar2semanticposs_cosmix.yaml --method cmmd-cosmix
 
-python adapt_cosmix.py --config_file configs/adaptation/nuscenes2semantickitti_cosmix.yaml --method cosmix
+python adapt_cosmix.py --config_file configs/adaptation/nuscenes2semanticposs/nuscenes2semanticposs_cosmix.yaml --method cosmix
 #python train_source.py --config_file configs/source/nuscenes2semanticposs.yaml
 # to sync cosmix run 
 # wandb sync /home/lamiaeel/projects/def-sdaniel/lamiaeel/CMMD/wandb/offline-run-20240405_113129-55l2yo7s -p 'SynLiDAR->SemanticKITTI'
